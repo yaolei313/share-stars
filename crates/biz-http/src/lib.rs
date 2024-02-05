f32bcf643fdc4cb0bab336802bfeb2e0590d2734
yaolei313 <yaolei313@gmail.com>
2024-02-06 01:00:22 +0800

    tmp commit

--- crates/biz-http/src/lib.rs
+++ crates/biz-http/src/lib.rs
@@ -1,7 +1,7 @@
 mod config;
 mod handler;
-mod vo;
 mod intercepter;
+mod vo;
 
 use axum::{
     error_handling::HandleErrorLayer,
@@ -18,13 +18,16 @@ use axum::{
 use axum_extra::{response::Html, TypedHeader};
 use headers::UserAgent;
 use serde::{Deserialize, Serialize};
-use std::{borrow::Borrow, collections::HashMap, time::Duration};
-use tower::Service;
+use std::{
+    borrow::{Borrow, Cow},
+    collections::HashMap,
+    time::Duration,
+};
 use tower::ServiceBuilder;
+use tower::{layer, Service};
 use tower_http::{timeout::TimeoutLayer, trace::TraceLayer};
 
 use crate::handler::login::*;
-
 
 pub fn init_router() -> Router {
     Router::new()
@@ -33,9 +36,32 @@ pub fn init_router() -> Router {
         .route("/login_by_sms", post(login_by_sms))
         .route("/profile/me", get(profile_me))
         .route("/profile/:user_id", get(profile))
-        // .layer(
-        //     ServiceBuilder::new()
-        //     .layer(TimeoutLayer::new(Duration::from_secs(3)))
-        //     .layer(TraceLayer::new_for_http()).into())
-        // .route_layer(middleware::from_fn(auth))
+        .layer(
+            ServiceBuilder::new()
+                // Handle errors from middleware
+                .layer(HandleErrorLayer::new(handle_error))
+                .load_shed()
+                .concurrency_limit(1024)
+                .timeout(Duration::from_secs(10))
+                .layer(TraceLayer::new_for_http())
+                .layer(middleware::from_fn(intercepter::auth)),
+        )
+}
+
+async fn handle_error(error: BoxError) -> impl IntoResponse {
+    if error.is::<tower::timeout::error::Elapsed>() {
+        return (StatusCode::REQUEST_TIMEOUT, Cow::from("request timed out"));
+    }
+
+    if error.is::<tower::load_shed::error::Overloaded>() {
+        return (
+            StatusCode::SERVICE_UNAVAILABLE,
+            Cow::from("service is overloaded, try again later"),
+        );
+    }
+
+    (
+        StatusCode::INTERNAL_SERVER_ERROR,
+        Cow::from(format!("Unhandled internal error: {error}")),
+    )
 }
